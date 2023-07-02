d328f0aa0a13d0f6abb1f6aa2f46bcb40c6dc38f
Beauclair Dongmo Ngnintedem <beauclair.dongmongnintedem@cofinpro.de>
2023-07-02 21:11:05 +0200

    Fix compiler error

--- Topics/Generics and Object/Initialize an object of a generic class/src/Main.java
+++ Topics/Generics and Object/Initialize an object of a generic class/src/Main.java
@@ -13,7 +13,7 @@ class Holder<T> {
 class Main {
     public static void main(String... args) {
         // initialize an instance of Holder to make the code below compiled
-        Holder...
+        Holder<String> holder = new Holder<>();
 
         // do not change the code below
         holder.set("This is an instance of String");
